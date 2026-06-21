a7657402d1b880596fa1334d79de752c83497684
Alessandro Trapani <alextra06@gmail.com>
2026-06-21 14:19:38 +0200

    Aggiunta filtri di protezione accessi

---
+++ src/main/java/filtro/FiltroAdmin.java
@@ -0,0 +1,50 @@
+package filtro;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import modello.Utente;
+
+@WebFilter("/admin/*")
+public class FiltroAdmin implements Filter {
+
+    public FiltroAdmin() {
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest richiesta = (HttpServletRequest) request;
+        HttpServletResponse risposta = (HttpServletResponse) response;
+
+        HttpSession sessione = richiesta.getSession(false);
+
+        Utente utente = null;
+
+        if (sessione != null) {
+            utente = (Utente) sessione.getAttribute("utenteLoggato");
+        }
+
+        if (utente == null || !utente.isAdmin()) {
+            risposta.sendRedirect(richiesta.getContextPath() + "/login");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/filtro/FiltroUtente.java
@@ -0,0 +1,54 @@
+package filtro;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import modello.Utente;
+
+@WebFilter({
+    "/checkout",
+    "/storico-ordini",
+    "/dettaglio-ordine"
+})
+public class FiltroUtente implements Filter {
+
+    public FiltroUtente() {
+    }
+
+    public void init(FilterConfig fConfig) throws ServletException {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest richiesta = (HttpServletRequest) request;
+        HttpServletResponse risposta = (HttpServletResponse) response;
+
+        HttpSession sessione = richiesta.getSession(false);
+
+        Utente utente = null;
+
+        if (sessione != null) {
+            utente = (Utente) sessione.getAttribute("utenteLoggato");
+        }
+
+        if (utente == null) {
+            risposta.sendRedirect(richiesta.getContextPath() + "/login");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+}
