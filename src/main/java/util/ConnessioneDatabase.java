01fe0373e45aa0459ee369c4341992232e8ec39a
Alessandro Trapani <alextra06@gmail.com>
2026-06-23 21:59:54 +0200

    Sostituzione DriverManager con DataSource

--- src/main/java/util/ConnessioneDatabase.java
+++ src/main/java/util/ConnessioneDatabase.java
@@ -1,25 +1,32 @@
 package util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class ConnessioneDatabase {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/skintrade";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static DataSource dataSource;
 
     static {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver MySQL non trovato.");
+            Context contesto = new InitialContext();
+            dataSource = (DataSource) contesto.lookup("java:comp/env/jdbc/skintrade");
+        } catch (NamingException e) {
+            System.out.println("Errore nel recupero del DataSource.");
             e.printStackTrace();
         }
     }
 
     public static Connection getConnessione() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        if (dataSource == null) {
+            throw new SQLException("DataSource non inizializzato.");
+        }
+
+        return dataSource.getConnection();
     }
 }
